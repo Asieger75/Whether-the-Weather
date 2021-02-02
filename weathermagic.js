aa8e93150c7c88625feda336959423e911fa7948
William Woodall <willwoodall@gmail.com>
2021-02-02 17:57:01 -0500

    added aditional forecast functionality

--- weathermagic.js
+++ weathermagic.js
@@ -1,7 +1,14 @@
 const API_key = 'c6dbc4ad804bfdc0473221ed78c36ae5';
 
-document.getElementById("searchBtn").addEventListener("click", function(){
-
+document.getElementById("searchBtn").addEventListener("click", function () {
+    var search = document.getElementById("searchField").value;
+    var searches = JSON.parse(localStorage.getItem(searches))
+    if (searches === null){
+        searches = [];
+    }
+    searches.unshift(search)
+    localStorage.setItem("searches", JSON.stringify(searches));
+    document.getElementById("searches")
     getWeather(document.getElementById("searchField").value);
     getForecast(document.getElementById("searchField").value);
 });
@@ -11,43 +18,71 @@ document.getElementById("searchBtn").addEventListener("click", function(){
 
 function getWeather(cityName) {
     console.log(cityName);
-    var currentUrl='http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + API_key;
+    var currentUrl = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + API_key;
     $.ajax({
         url: currentUrl,
         type: "GET",
-        success: function(result){
+        success: function (result) {
             console.log(result);
             console.log(result.coord);
-            document.getElementById("name").innerHTML=result.name;
-            document.getElementById("temp").innerHTML=result.main.temp;
-            document.getElementById("humidity").innerHTML=result.main.humidity;
-            document.getElementById("speed").innerHTML=result.wind.speed;
+            document.getElementById("name").innerHTML = result.name;
+            document.getElementById("temp").innerHTML = result.main.temp;
+            document.getElementById("humidity").innerHTML = result.main.humidity;
+            document.getElementById("speed").innerHTML = result.wind.speed;
+            document.getElementById("weatherIcon").src = 'http://openweathermap.org/img/wn/' + result.weather[0].icon + '@2x.png';
+            var lat = result.coord.lat;
+            var lon = result.coord.lon;
+            console.log("lat and lon", lat, lon);
 
-            document.getElementById("weatherIcon").src= 'http://openweathermap.org/img/wn/' + result.weather[0].icon + '@2x.png';
+            var currentUrl = 'http://api.openweathermap.org/data/2.5/uvi?lat=' + lat +'&lon=' + lon + '&appid=' + API_key;
+            $.ajax({
+                url: currentUrl,
+                type: "GET",
+                success: function (result) {
+                    console.log("result: ", result);
+                    console.log(result.value);
+                    document.getElementById("uv").innerHTML = result.value;
+        
+        
+                }
+        
+        
+        
+        
+            })
+        
+            //getUVIndex(result.data.coord.lat, long); yada
 
         }
+
     })
-    //localStorage.setItem(JSON.stringify(cityName)) THIS BORKS THE WHOLE THINGS AS-IS
+
+
+
+    //localStorage.setItem(JSON.stringify(cityName)) THIS BORKS THE WHOLE THING AS-IS
 }
 
 function getForecast(cityName) {
     console.log("hit");
-    var currentUrl='http://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&appid=' + API_key;
+    var currentUrl = 'http://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&appid=' + API_key;
     $.ajax({
         url: currentUrl,
         type: "GET",
-        success: function(result){
+        success: function (result) {
             for (var i = 0; i <= 32; i += 8) {
                 console.log(i);
                 console.log(result);
-                document.getElementById("temp" + i.toString()).innerHTML=result.list[i].main.temp;
-                document.getElementById("humidity"+ i.toString()).innerHTML=result.list[i].main.humidity;
-                document.getElementById("date" + i.toString()).innerHTML=result.list[i].dt_txt;
-                document.getElementById("weatherIcon" + + i.toString()).src= 'http://openweathermap.org/img/wn/' + result.list[i].weather[0].icon + '@2x.png';
+                document.getElementById("temp" + i.toString()).innerHTML = result.list[i].main.temp;
+                document.getElementById("humidity" + i.toString()).innerHTML = result.list[i].main.humidity;
+                document.getElementById("date" + i.toString()).innerHTML = result.list[i].dt_txt;
+                document.getElementById("weatherIcon" + + i.toString()).src = 'http://openweathermap.org/img/wn/' + result.list[i].weather[0].icon + '@2x.png';
             }
         }
     })
 }
+
+//function getUVIndex(lat, long){ }
+
 
 
 
